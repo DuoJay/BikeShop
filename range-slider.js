8d6f15311a1ce047a3b566c673b4239ae95bca0f
DuoJay <82710377+DuoJay@users.noreply.github.com>
2022-10-03 12:54:59 +0200

    fix range slider
    -flexbox for the range slider
    -color and look of select input on apples

--- range-slider.js
+++ range-slider.js
@@ -8,16 +8,14 @@ filterForm.addEventListener('submit', e => {
 });
 
 const getVals = () => {
-  // ZÍSKÁNÍ HODNOT Z POSUVNÍKŮ
   let slide1 = parseFloat(document.querySelectorAll('.range-slide')[0].value);
   let slide2 = parseFloat(document.querySelectorAll('.range-slide')[1].value);
-  // POSUVNÍKY SE NESTŘETNOU, JE POTŘEBA ROZHODNOUT, KTERÝ MÁ VĚTSÍ HODNOTU
+
   if (slide1 > slide2) {
     let tmp = slide2;
     slide2 = slide1;
     slide1 = tmp;
   }
-  //   UDÁNÍ HODNOT PRO MIN A MAX LABEL
   const minValue = document.querySelector('#min-value');
   const maxValue = document.querySelector('#max-value');
   minValue.value = slide1;
